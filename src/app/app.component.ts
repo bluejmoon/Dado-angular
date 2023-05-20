79a144852ec9f904097e7f4d9061f54a07d5bf33
Valentina Garzón Gómez <126628829+bluejmoon@users.noreply.github.com>
2023-05-19 19:13:33 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'herencia_angular';
+  // title = 'herencia_angular';
+  valor1:number;
+  valor2:number;
+  valor3:number;
+  resultado:string="";
+  constructor(){
+    this.valor1 = this.numeroAleatorio();
+    this.valor2 = this.numeroAleatorio();
+    this.valor3 = this.numeroAleatorio();
+  }
+  numeroAleatorio(){
+    return Math.trunc(Math.random()*6)+1
+  }
+
+  lanzar(){
+    this.valor1 = this.numeroAleatorio();
+    this.valor2 = this.numeroAleatorio();
+    this.valor3 = this.numeroAleatorio();
+    this.resultadoApuesta();
+  }
+
+  resultadoApuesta(){
+    if(this.valor1 == this.valor2 && this.valor1 == this.valor3 && this.valor2 == this.valor3)
+    this.resultado = "¡Usted es ganador!"
+
+    else
+    this.resultado = "¡Usted es perdedor!"
+  }
 }
+
